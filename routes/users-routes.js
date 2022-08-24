b1c83b1530d698832185ff5748e86a356a092864
willmclellarn <willmclellarn@gmail.com>
2022-08-24 18:18:02 -0500

    users validation

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { check } = require("express-validator");
 
 const usersController = require("../controllers/users-controllers");
 
@@ -6,7 +7,15 @@ const router = express.Router();
 
 router.get("/", usersController.getUsers);
 
-router.post("/signup", usersController.signup);
+router.post(
+  "/signup",
+  [
+    check("name").not().isEmpty(),
+    check("email").isLength({ min: 5 }),
+    check("password").not().isEmpty(),
+  ],
+  usersController.signup
+);
 
 router.post("/login", usersController.login);
 
