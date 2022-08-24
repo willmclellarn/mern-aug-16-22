0e102dd4e9e97b672add6708d189464964093760
willmclellarn <willmclellarn@gmail.com>
2022-08-24 17:30:51 -0500

    finished routing

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,5 +1,4 @@
 const { v4: uuidv4 } = require("uuid");
-
 const HttpError = require("../models/http-error");
 
 // Database Modeling, it's pretend data as if there were a DB, very smart
@@ -18,16 +17,24 @@ const getUsers = (req, res, next) => {
 
 const signup = (req, res, next) => {
   const { name, email, password } = req.body;
+
+  const hasUser = DUMMY_USERS.find((u) => {
+    return u.email === email;
+  });
+  if (hasUser) {
+    throw new HttpError("Could not create user, email already exists.", 422);
+  }
+
   const createdUser = {
     id: uuidv4(),
-    name,
+    name, // name: name
     email,
     password,
   };
 
   DUMMY_USERS.push(createdUser);
 
-  res.send(201).json({ user: createdUser });
+  res.status(201).json({ user: createdUser });
 };
 
 const login = (req, res, next) => {
@@ -37,7 +44,7 @@ const login = (req, res, next) => {
   const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
   // if not, throw an error (really just send them to sign up?)
   if (!identifiedUser || identifiedUser.password !== password) {
-    throw new HttpError("This user does not exist", 401);
+    throw new HttpError("Something went wrong - you did not login", 401);
   } else {
     res.json({ message: "logged in" });
   }
