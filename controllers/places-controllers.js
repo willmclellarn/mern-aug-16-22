7389669644c8e6015c14f00ca3d55589827ce7e6
willmclellarn <willmclellarn@gmail.com>
2022-08-24 19:29:11 -0500

    adding return next to stop execution

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -57,7 +57,9 @@ const createPlace = async (req, res, next) => {
   if (!errors.isEmpty()) {
     console.log(errors);
     // always need to next errors in async code
-    next(new HttpError("invalid inputs passed, please check your data", 422));
+    return next(
+      new HttpError("invalid inputs passed, please check your data", 422)
+    );
   }
 
   const { title, description, address, creator } = req.body;
